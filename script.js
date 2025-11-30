c082a523ad79940de01c47ff11b96949dafe5c19
asimkhan85 <asimkhan83437@gmail.com>
2025-11-30 15:30:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,38 @@
+const images = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png"];  // apni 6 photos
 let current = 0;
+const img = document.getElementById("mainImage");
 
-const images = [
-  "1.png",
-  "2.png",
-  "3.png",
-  "4.png",
-  "5.png",
-  "6.png"
-];
+function showImage(index) {
+  img.src = images[index];
+}
 
-const img = document.getElementById("img");
-
-img.src = images[current];
-
-img.addEventListener("touchstart", startTouch, false);
-img.addEventListener("touchend", endTouch, false);
-
+// For touch events (mobile)
 let startX = 0;
-
-function startTouch(e) {
-  startX = e.changedTouches[0].screenX;
-}
-
-function endTouch(e) {
-  let endX = e.changedTouches[0].screenX;
-
-  if (endX < startX - 50) {
-    nextImage();
+img.addEventListener("touchstart", e => {
+  startX = e.touches[0].clientX;
+});
+img.addEventListener("touchend", e => {
+  let endX = e.changedTouches[0].clientX;
+  if (endX < startX - 30) {
+    current = (current + 1) % images.length;
+    showImage(current);
+  } else if (endX > startX + 30) {
+    current = (current - 1 + images.length) % images.length;
+    showImage(current);
   }
-  if (endX > startX + 50) {
-    prevImage();
+});
+
+// For mouse drag (desktop)
+img.addEventListener("mousedown", e => {
+  startX = e.clientX;
+});
+img.addEventListener("mouseup", e => {
+  let endX = e.clientX;
+  if (endX < startX - 30) {
+    current = (current + 1) % images.length;
+    showImage(current);
+  } else if (endX > startX + 30) {
+    current = (current - 1 + images.length) % images.length;
+    showImage(current);
   }
-}
-
-function nextImage() {
-  current = (current + 1) % images.length;
-  img.src = images[current];
-}
-
-function prevImage() {
-  current =
-    (current - 1 + images.length) % images.length;
-  img.src = images[current];
-}
+});
